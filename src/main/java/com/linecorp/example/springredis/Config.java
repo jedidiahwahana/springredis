cfdbb67505e1d8b81811cd4ecc84dce478fd62ca
Jedidiah Wahana <jedidiah.wahana@gmail.com>
2017-02-22 18:29:01 +0700

    feat: insert data from app and can be deployed in Bluemix

--- src/main/java/com/linecorp/example/springredis/Config.java
+++ src/main/java/com/linecorp/example/springredis/Config.java
@@ -8,15 +8,30 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
+import org.json.JSONObject;
+import org.json.JSONArray;
+
 @Configuration
 public class Config
 {
     
 	@Bean
     JedisConnectionFactory jedisConnectionFactory() {
+        String sServices=System.getenv("VCAP_SERVICES");
+        JSONObject jServices=new JSONObject(sServices);
+        JSONArray aRedis=jServices.getJSONArray("compose-for-redis");
+        JSONObject jRedis=aRedis.getJSONObject(0);
+        JSONObject jCredentials=jRedis.getJSONObject("credentials");
+        String dbUrl=jCredentials.getString("uri");
+        String[] details = dbUrl.split("[:@]");
+        
         JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
-        jedisConFactory.setHostName("localhost");
-        jedisConFactory.setPort(6379);
+//        jedisConFactory.setHostName("localhost");
+//        jedisConFactory.setPort(6379);
+
+        jedisConFactory.setHostName(details[3]);
+        jedisConFactory.setPort(Integer.parseInt(details[4]));
+        jedisConFactory.setPassword(details[2]);
         return jedisConFactory;
     }
     
--- src/main/java/com/linecorp/example/springredis/Controller.java
+++ src/main/java/com/linecorp/example/springredis/Controller.java
@@ -1,8 +1,7 @@
 
 package com.linecorp.example.springredis;
 
-import java.util.Map;
-import java.util.List;
+import java.util.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,21 +26,50 @@ public class Controller
         System.out.println("3----------");
         System.out.println("HasKey Result Simple: " + template.hasKey("name"));
         System.out.println("4----------");
-        System.out.println("HasKey Result Hash Map: " + template.hasKey("user:1"));
+        System.out.println("HasKey Result Hash Map: " + template.hasKey("tutorialspoint"));
         System.out.println("5----------");
-        System.out.println("HasKey Result List: " + template.hasKey("list"));
+        System.out.println("HasKey Result List: " + template.hasKey("tutorials"));
         System.out.println("6----------");
+        
         Object testVal1 = template.opsForValue().get("name");
         System.out.println("TestVal1 returned from Redis: " + testVal1);
-        Map<Object, Object> testVal2 = template.opsForHash().entries("user:1");
+        
+        Map<Object, Object> testVal2 = template.opsForHash().entries("tutorialspoint");
         for (Object key : testVal2.keySet())
             System.out.println("TestVal2 key: " + key + " TestVal2 value: " + testVal2.get(key));
-        List<Object> testVal3 = template.opsForList().range("list", 0, template.opsForList().size("list"));
+        
+        List<Object> testVal3 = template.opsForList().range("tutorials", 0, template.opsForList().size("tutorials"));
         for (int i=0; i<testVal3.size(); i++)
             System.out.println("TestVal3 value: " + testVal3.get(i));
+
         template.opsForValue().set("number", "1234567890");
-        System.out.println("HasKey Result List: " + template.hasKey("number"));
+        System.out.println("HasKey Result Simple: " + template.hasKey("number"));
         System.out.println("7----------");
+        System.out.println("Simple Input: " + template.opsForValue().get("number"));
+        System.out.println("8----------");
+        
+        Map newMap = new HashMap();
+        newMap.put("Brand 1", "Nike");
+        newMap.put("Brand 2", "Adidas");
+        newMap.put("Brand 3", "Under Armour");
+        template.opsForHash().putAll("Sport", newMap);
+        System.out.println("HasKey Result Hash Map: " + template.hasKey("Sport"));
+        System.out.println("9----------");
+        Map<Object, Object> hashVal2 = template.opsForHash().entries("Sport");
+        for (Object key : hashVal2.keySet())
+            System.out.println("Hash Input key: " + key + " TestVal2 value: " + hashVal2.get(key));
+
+        
+        List newList = new ArrayList();
+        newList.add("Apple");
+        newList.add("Samsung");
+        newList.add("Xiaomi");
+        template.opsForList().leftPushAll("Electronics", newList);
+        System.out.println("HasKey Result List: " + template.hasKey("Electronics"));
+        System.out.println("10----------");
+        List<Object> listVal3 = template.opsForList().range("Electronics", 0, template.opsForList().size("Electronics"));
+        for (int i=0; i<listVal3.size(); i++)
+            System.out.println("TestVal3 value: " + listVal3.get(i));
         
         return new ResponseEntity<String>("{}", HttpStatus.OK);
     }
